b7d7d98e9b42372ca62248aa321221248e72e24b
Amadej Pevec <amadej.pevec@gmail.com>
2020-04-14 13:34:08 +0200

    Get network by id instead of name

--- common/v2/services/Store/Network/NetworkProvider.tsx
+++ common/v2/services/Store/Network/NetworkProvider.tsx
@@ -1,12 +1,14 @@
 import React, { useContext, createContext } from 'react';
 
 import { Network, NetworkId, NodeType, NodeOptions, LSKeys } from 'v2/types';
+
 import { DataContext } from '../DataManager';
+import { getNetworkById } from './helpers';
 
 export interface INetworkContext {
   networks: Network[];
   updateNetwork(id: NetworkId, item: Network): void;
-  getNetworkByName(name: string): Network | undefined;
+  getNetworkById(id: NetworkId): Network | undefined;
   getNetworkByChainId(chainId: number): Network | undefined;
   addNodeToNetwork(node: NodeOptions, network: Network): void;
   createWeb3Node(networkId: NetworkId): NodeOptions;
@@ -21,12 +23,10 @@ export const NetworkProvider: React.FC = ({ children }) => {
   const state: INetworkContext = {
     networks,
     updateNetwork: model.update,
-    getNetworkByName: name => {
-      return networks.find((network: Network) => network.name === name);
-    },
     getNetworkByChainId: chainId => {
       return networks.find((network: Network) => network.chainId === chainId);
     },
+    getNetworkById: id => getNetworkById(id, networks),
     addNodeToNetwork: (node: NodeOptions, network: Network) => {
       const n = {
         ...network,
