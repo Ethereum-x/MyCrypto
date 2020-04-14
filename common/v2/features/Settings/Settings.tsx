11d2292b3fca6d618d0ec666a6830f16e9744a3e
Matej Lubej <matej.lubej@poviolabs.com>
2020-04-14 13:07:04 +0200

    Modify navbar for settings

--- common/v2/features/Settings/Settings.tsx
+++ common/v2/features/Settings/Settings.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+import { Heading } from '@mycrypto/ui';
 import {
   AddressBookContext,
   NetworkContext,
@@ -6,14 +8,38 @@ import {
   SettingsContext,
   StoreContext
 } from 'v2/services/Store';
-import { AccountList, FlippablePanel, MobileNavBar } from 'v2/components';
+import { AccountList, FlippablePanel, Mobile, MobileNavBar, Desktop } from 'v2/components';
 import { NetworkId } from 'v2/types';
 import { CustomNodeConfig } from 'v2/types/node';
 import { DEFAULT_NETWORK, IS_ACTIVE_FEATURE } from 'v2/config';
+import { BREAK_POINTS } from 'v2/theme';
+import translate from 'v2/translations';
 
 import NetworkNodes from './components/NetworkNodes';
 import AddOrEditNetworkNode from './components/AddOrEditNetworkNode';
 import { AddressBookPanel, AddToAddressBook, GeneralSettings, DangerZone } from './components';
+import settingsIcon from 'common/assets/images/icn-settings.svg';
+
+const SettingsHeading = styled(Heading)`
+  display: flex;
+  align-items: center;
+  margin-bottom: 22px;
+  color: #163150;
+`;
+
+const SettingsHeadingIcon = styled.img`
+  margin-right: 12px;
+`;
+
+const StyledLayout = styled.div`
+  width: 960px;
+  @media (max-width: ${BREAK_POINTS.SCREEN_SM}) {
+    width: 100%;
+  }
+  .Layout-content {
+    padding: 0;
+  }
+`;
 
 function renderAccountPanel() {
   const { accounts } = useContext(StoreContext);
@@ -120,32 +146,44 @@ export default function Settings() {
   const currentTab = tabOptions[tab];
 
   return (
-    <>
-      <MobileNavBar>
-        <div
-          className={`tab ${tab === 'accounts' ? 'active' : ''}`}
-          onClick={() => setTab('accounts')}
-        >
-          <h6>Accounts</h6>
-        </div>
-        <div
-          className={`tab ${tab === 'addresses' ? 'active' : ''}`}
-          onClick={() => setTab('addresses')}
-        >
-          <h6>Addresses</h6>
-        </div>
-        <div className="w-100" />
-        <div className={`tab ${tab === 'nodes' ? 'active' : ''}`} onClick={() => setTab('nodes')}>
-          <h6>Network & Nodes</h6>
-        </div>
-        <div
-          className={`tab ${tab === 'general' ? 'active' : ''}`}
-          onClick={() => setTab('general')}
-        >
-          <h6>General</h6>
-        </div>
-      </MobileNavBar>
-      <>{currentTab}</>
-    </>
+    <StyledLayout>
+      <Mobile>
+        <MobileNavBar>
+          <div
+            className={`tab ${tab === 'accounts' ? 'active' : ''}`}
+            onClick={() => setTab('accounts')}
+          >
+            <h6>Accounts</h6>
+          </div>
+          <div
+            className={`tab ${tab === 'addresses' ? 'active' : ''}`}
+            onClick={() => setTab('addresses')}
+          >
+            <h6>Addresses</h6>
+          </div>
+          <div className="w-100" />
+          <div className={`tab ${tab === 'nodes' ? 'active' : ''}`} onClick={() => setTab('nodes')}>
+            <h6>Network & Nodes</h6>
+          </div>
+          <div
+            className={`tab ${tab === 'general' ? 'active' : ''}`}
+            onClick={() => setTab('general')}
+          >
+            <h6>General</h6>
+          </div>
+        </MobileNavBar>
+        <>{currentTab}</>
+      </Mobile>
+      <Desktop>
+        <SettingsHeading>
+          <SettingsHeadingIcon src={settingsIcon} alt="Settings" />
+          {translate('SETTINGS_HEADING')}
+        </SettingsHeading>
+        {renderAccountPanel()}
+        {renderAddressPanel()}
+        {renderNetworkNodes()}
+        {renderGeneralSettingsPanel()}
+      </Desktop>
+    </StyledLayout>
   );
 }
